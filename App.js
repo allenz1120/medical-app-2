e1880b8f893299dbf2ff0848c758b48fa4412cce
allenz1120 <allenzou@bu.edu>
2022-03-29 11:32:51 -0400

    working base app

---
+++ App.js
@@ -0,0 +1,25 @@
+import 'react-native-gesture-handler';
+import React, { useEffect, useState } from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { LoginScreen, HomeScreen } from './src/screens'
+// import {decode, encode} from 'base-64'
+// if (!global.btoa) {  global.btoa = encode }
+// if (!global.atob) { global.atob = decode }
+
+const Stack = createStackNavigator();
+
+export default function App() {
+
+  // const [loading, setLoading] = useState(true)
+  // const [user, setUser] = useState(null)
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen name="Home" component={HomeScreen}/>
+          <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
