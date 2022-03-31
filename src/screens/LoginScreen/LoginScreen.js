005e8dd11e7ee6f8eab164e80b2e6957e1030778
Alex Prior <47067268+priorpeak@users.noreply.github.com>
2022-03-31 10:44:59 -0400

    Added existing user checking to login

--- src/screens/LoginScreen/LoginScreen.js
+++ src/screens/LoginScreen/LoginScreen.js
@@ -1,40 +1,63 @@
-import React, { Component } from 'react'
-import { Text, View, TouchableOpacity } from 'react-native'
-import styles from './styles';
-import * as Google from 'expo-google-app-auth';
+import React, { Component } from "react";
+import { Text, View, TouchableOpacity } from "react-native";
+import styles from "./styles";
+import * as Google from "expo-google-app-auth";
+import axios from "axios";
 
+export default function LoginScreen({ navigation }) {
+  async function signInWithGoogleAsync() {
+    try {
+      const result = await Google.logInAsync({
+        androidClientId:
+          "293213095726-su3o0t6c6t2irst7k9bj722uh7co8rf4.apps.googleusercontent.com",
+        // behavior:'web',
+        iosClientId:
+          "293213095726-oacojpdlnpq1obdk52ba9to5ccaiseqi.apps.googleusercontent.com",
+        scopes: ["profile", "email"],
+      });
 
-
-  
-export default function LoginScreen({navigation}) {
-    async function signInWithGoogleAsync() {
-        try {
-          const result = await Google.logInAsync({
-            androidClientId: '293213095726-su3o0t6c6t2irst7k9bj722uh7co8rf4.apps.googleusercontent.com',
-            // behavior:'web',
-            iosClientId: '293213095726-oacojpdlnpq1obdk52ba9to5ccaiseqi.apps.googleusercontent.com',
-            scopes: ['profile', 'email'],
+      if (result.type === "success") {
+        // console.log(result);
+        var email = result.user.email;
+        // console.log(email);
+        // Query db to see if user is already registered
+        var requestUri = "http://3.92.181.151:8000/users/";
+        console.log(requestUri);
+        axios
+          .get(requestUri)
+          .then((response) => {
+            // console.log(response["data"]);
+            var userlist = response["data"];
+            userlist.forEach((user, i) => {
+              console.log(user.email);
+              if (user.email == email) {
+                navigation.navigate("Home");
+              }
+            });
+            navigation.navigate("Register");
+          })
+          .catch((error) => {
+            console.log(error);
           });
-      
-          if (result.type === 'success') {
-            navigation.navigate('Home')
-            return result.accessToken;
-          } else {
-            return { cancelled: true };
-          }
-        } catch (e) {
-          return { error: true };
-        }
-      }
-    return (
-            <View>
-                <TouchableOpacity
-                    style={styles.button} onPress= {() =>signInWithGoogleAsync()}>
-                    <Text style={styles.buttonTitle}>Log in with Google</Text>
-                </TouchableOpacity>
-            </View>
 
-    )
+        return result.accessToken;
+      } else {
+        return { cancelled: true };
+      }
+    } catch (e) {
+      return { error: true };
+    }
+  }
+  return (
+    <View>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => signInWithGoogleAsync()}
+      >
+        <Text style={styles.buttonTitle}>Log in with Google</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 // export default LoginScreen
